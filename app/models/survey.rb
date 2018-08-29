133813509366caba2b6df5b3fe03fbea11ccad64
bohdankutkovy <kutkovybohdan@gmail.com>
2018-08-29 22:30:43 +0300

    small fix

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,8 +4,8 @@ class Survey < ApplicationRecord
   validates_presence_of :guest, message: "Виберіть кількість гостей!"
   validates_presence_of :event, message: "Виберість список подій!"
 
-  GUESTS = [1, 2, 3, 4]
-  EVENTS = ['Всі заходи', 'Вінчання', 'Вечірку']
+  GUESTS = (1..10).to_a.freeze
+  EVENTS = ['Всі заходи', 'Вінчання', 'Вечірку'].freeze
 
   # validates :guest, inclusion: { in: GUESTS }
   # validates :event, inclusion: { in: EVENTS }
