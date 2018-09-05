fb432f53482a2192f848fa021bd359e0ee75242e
bohdankutkovy <kutkovybohdan@gmail.com>
2018-09-05 22:11:49 +0300

    fix

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,7 @@
 class Survey < ApplicationRecord
-  validates_presence_of :name,  message: "Будьласка, заповніть своє ім'я!"
+  validates_presence_of :name,  message: "Будь-ласка, заповніть своє ім'я!"
   # validates_presence_of :email, message: "Будьлкака, введіть свій email!"
-  validates_presence_of :phone_number, message: "Будьлкака, введіть свій мобільний!"
+  validates_presence_of :phone_number, message: "Будь-ласка, введіть свій мобільний!"
   validates_presence_of :guest, message: "Виберіть кількість гостей!"
   validates_presence_of :event, message: "Виберість список подій!"
 
